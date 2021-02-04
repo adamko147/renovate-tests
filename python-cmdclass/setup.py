ee1c7df0ac82f3633c334c394ee3a469382921af
Adam Horacek <adam.horacek@gmail.com>
2021-02-04 14:32:06 +0100

    first commit

---
+++ python-cmdclass/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+from distutils.command.sdist import sdist as _sdist
+
+class sdist(_sdist):
+    """Custom script run during setup.py sdist"""
+
+
+setup(
+    name='my-package',
+    version='1.0.0',
+    packages=find_packages(),
+    install_requires=[
+        'aiohttp==3.7.2',
+    ],
+    cmdclass={
+        'sdist': sdist
+    })
---
+++ python-no-setup-call/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+from distutils.command.sdist import sdist as _sdist
+
+
+# setup(
+#     name='my-package',
+#     version='1.0.0',
+#     packages=find_packages(),
+#     install_requires=[
+#         'aiohttp==3.7.3',
+#     ])
